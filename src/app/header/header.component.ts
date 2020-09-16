7c92f16dd14cf509123f180f5e18d655147a8cda
Carlos Duarte <carlosapaduarte@gmail.com>
2020-09-16 12:51:09 +0100

    move language selection to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,35 @@
-import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { ThemeService } from '../theme.service';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { Subscription } from "rxjs";
+import { ThemeService } from "../theme.service";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-
   private sub: Subscription;
 
   isHomePage: boolean;
+  selectedLang: string;
 
   constructor(
-    private readonly theme: ThemeService, 
+    private readonly theme: ThemeService,
     private readonly cd: ChangeDetectorRef,
-    private readonly router: Router
+    private readonly router: Router,
+    public readonly translate: TranslateService
   ) {
-    this.isHomePage = !location.pathname.startsWith('/results');
+    this.isHomePage = !location.pathname.startsWith("/results");
+
+    this.selectedLang = this.translate.currentLang;
   }
 
   ngOnInit(): void {
     this.sub = this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.isHomePage = !location.pathname.startsWith('/results');
+        this.isHomePage = !location.pathname.startsWith("/results");
       }
     });
   }
@@ -34,15 +38,26 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
+  changeLanguage(): void {
+    if (this.selectedLang === "Portuguese") {
+      this.selectedLang = "English";
+    } else {
+      this.selectedLang = "Portuguese";
+    }
+
+    this.translate.use(this.selectedLang);
+    localStorage.setItem("language", this.selectedLang);
+  }
+
   toggleLightDarkTheme(): void {
     if (this.theme.isDarkTheme()) {
       this.theme.setLightTheme();
-      localStorage.setItem('theme', 'light');
+      localStorage.setItem("theme", "light");
     } else {
       this.theme.setDarkTheme();
-      localStorage.setItem('theme', 'dark');
+      localStorage.setItem("theme", "dark");
     }
-    
+
     this.cd.detectChanges();
   }
 }
