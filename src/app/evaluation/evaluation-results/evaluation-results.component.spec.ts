4518cc0e2bbdefac1bff02efe3c16389bb14ffab
João Vicente <joao.vicente@campus.ul.pt>
2020-12-15 16:05:43 +0000

    Updated @angular/cli and @angular/core to v11

--- src/app/evaluation/evaluation-results/evaluation-results.component.spec.ts
+++ src/app/evaluation/evaluation-results/evaluation-results.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EvaluationResultsPageComponent } from './evaluation-results.component';
 
@@ -6,7 +6,7 @@ describe('EvaluationResultsPageComponent', () => {
   let component: EvaluationResultsPageComponent;
   let fixture: ComponentFixture<EvaluationResultsPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EvaluationResultsPageComponent ]
     })
--- src/app/evaluation/webpage-code/webpage-code.component.spec.ts
+++ src/app/evaluation/webpage-code/webpage-code.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WebpageCodePageComponent } from './webpage-code.component';
 
@@ -6,7 +6,7 @@ describe('WebpageCodePageComponent', () => {
   let component: WebpageCodePageComponent;
   let fixture: ComponentFixture<WebpageCodePageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WebpageCodePageComponent ]
     })
