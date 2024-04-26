7444cf6847f516412d122f95005d60c0b2729e4e
Filipe Faustino <filipe.faustino@lbc-global.com>
2024-04-26 18:13:29 +0100

    dark-mode styles + table image titles

--- src/components/Atoms/Icon/index.js
+++ src/components/Atoms/Icon/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./style.css";
 import "./icon.css";
+import { useTranslation } from "react-i18next";
 
 const Icon = ({ name }) => {
-  return <i className={`icon-${name}`}></i>;
+  const { t } = useTranslation();
+  
+  return <i title={t(`RESULTS.results.image_title.${name}`)} className={`icon-${name}`}></i>;
 };
 
 export { Icon };
