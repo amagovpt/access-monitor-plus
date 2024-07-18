d0d588f95785bd915c75d333bd2ae1424b45bc8b
Carlos Duarte <caduarte@edu.ulisboa.pt>
2024-07-18 09:18:32 +0100

    Remove leftovers from angular version

--- src/app/global/loading/loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.scss']
-})
-export class LoadingComponent {}
